987a4b4b38287f7df9c74e88e878ee3ec8eaf64a
James Wallace <sausages@gmail.com>
2017-06-10 16:52:16 -0400

    Fixed the variable name for the request!

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@ $app->get('/privacypolicy', function (Request $r, Response $res) {
 });
 
 $app->post('/', function (Request $req, Response $res) {
-	$parsedBody = $request->getParsedBody();
+	$parsedBody = $req->getParsedBody();
 
 	$res->getBody()->write(print_r($parsedBody, true));
 });
