1134eaf2919b55840112624f681f38467837d5e4
James Wallace <sausages@gmail.com>
2017-06-10 14:07:08 -0400

    Updated the index.php file!

--- src/index.php
+++ src/index.php
@@ -4,7 +4,13 @@ require '../vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app = new \Slim\App;
+$config = [
+	'settings' => [
+		'displayErrorDetails' => true
+	]
+];
+
+$app = new \Slim\App($config);
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
 	$name = $request->getAttribute('name');
