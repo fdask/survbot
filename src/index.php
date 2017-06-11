f3eef23ae4f44ca6ede52b4a9267dc15837bec19
James Wallace <sausages@gmail.com>
2017-06-11 10:44:40 -0400

    Updated the index file to iterate over 'event' and 'message' blocks in the Facebook response.

--- src/index.php
+++ src/index.php
@@ -26,25 +26,37 @@ $app->get('/privacypolicy', function (Request $r, Response $res) {
 });
 
 $app->post('/', function (Request $req, Response $res) {
+	// get the pageId we're hooked up to!
+	$pageId = Settings::get_ini_value('facebook', 'page_id');
+
+	// parse out the posted message
 	$parsedBody = $req->getParsedBody();
 
 	// parse out the message and send a reply!
-	$recipientId = $parsedBody['entry'][0]['messaging'][0]['recipient']['id'];
+	if (isset($parsedBody['entry']) && !empty($parsedBody['entry'])) {
+		foreach ($parsedBody['entry'] as $entry) {
+			if (isset($entry['messaging']) && !empty($entry['messaging'])) {
+				foreach ($entry['messaging'] as $message) {
+					if (isset($message['message'])) {
+						$recipientId = $message['recipient']['id'];
 
-	/**
-	if ($recipientId == "629947963791983") {
-		$senderId = $parsedBody['entry'][0]['messaging'][0]['sender']['id'];
-		$message = $parsedBody['entry'][0]['messaging'][0]['message']['text'];
+						if ($recipientId == $pageId) {
+							$senderId = $message['sender']['id'];
+							$message = $message['message']['text'];
 
-		error_log("User $senderId said '$message'");
+							error_log("User $senderId said '$message'");
 
-		if (FB::sendMessage($senderId, "BEEP!")) {
-			error_log("Reply successfully sent");
-		} else {
-			error_log("Error sending reply!");
+							if (FB::sendMessage($senderId, "BEEP!")) {
+								error_log("Reply successfully sent");
+							} else {
+								error_log("Error sending reply!");
+							}
+						} 
+					}
+				}
+			}
 		}
 	}
-	**/
 });
 
 $app->run();
